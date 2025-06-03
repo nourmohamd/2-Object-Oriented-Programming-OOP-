5146fc791ea998a9f975cb4f210b295f76d79593
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-06-03 23:17:14 +0300

    Upload 012_Friend_Function

---
+++ 012_Friend_Function/012_Friend_Function.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+// Ex1:
+class Num {
+private:
+    int a, b;
+public:
+    Num():a(0), b(0) {
+        cout<<"Empty Constructor"<<endl;
+    }
+    Num(int aa, int bb):a(aa), b(bb) {
+        cout<<"Paramitrize Constructor"<<endl;
+    }
+    friend int sum_a(Num n1, Num n2);
+};
+int sum_a(Num n1, Num n2) {
+    return n1.a + n2.a;
+}
+// Ex2:
+class Rectangle {
+private:
+    int width, height;
+public:
+    void set_width_height(int w, int h);
+    int area() {
+        return (width + height);
+    }
+    friend Rectangle duplicate(Rectangle n);
+};
+void Rectangle::set_width_height(int w, int h) {
+    cout<<"Set Values For Member Private"<<endl;
+    width = w;
+    height = h;
+}
+Rectangle duplicate(Rectangle n) {
+    Rectangle t1;
+    t1.width = n.width*2;
+    t1.height = n.height*2;
+    return t1;
+}
+int main() {
+    // Friend Function
+    /*
+        What is this?
+        =============
+        This idea make for members private i can't see them out class
+        for this make friend/method function for i can see them out class
+    */
+
+    // Ex1: Called sum_a For Class Num:
+    Num n1(10, 20), n2(20, 10);
+    cout<<sum_a(n1, n2)<<endl;
+    /*
+        Output:
+        =======
+        Paramitrize Constructor
+        Paramitrize Constructor
+        30
+    */
+
+    // Ex2: Called duplicate For Class Rectangle
+    Rectangle recta, rectb;
+    recta.set_width_height(30, 50);
+    cout<<recta.area()<<endl;
+    rectb = duplicate(recta);
+    cout<<rectb.area()<<endl;
+    /*
+        Output:
+        =======
+        Set Values For Member Private
+        80
+        160
+    */
+
+
+
+    return 0;
+    system("PAUSE");
+}
